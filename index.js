2a6a655669958c72429acf28c9293d5ae36fc8cb
Ariful Islam Shuvo <arifulislams063@gmail.com>
2022-05-24 14:38:06 +0600

    created parts API

--- index.js
+++ index.js
@@ -5,10 +5,32 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
 
-
+//middleware 
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h6rs5.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        await client.connect();
+        const partsCollection = client.db('bicycle_parts').collection('parts')
+
+        app.get('/partses', async (req, res) => {
+            const query = {};
+            const cursor = partsCollection.find(query);
+            const parts = await cursor.toArray();
+            res.send(parts);
+        });
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) =>{
     res.send('Hellow ');
