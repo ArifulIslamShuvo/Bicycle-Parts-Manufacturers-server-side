502b7d634f408788e9a99f97760d164670db1be9
Ariful Islam Shuvo <arifulislams063@gmail.com>
2022-05-27 11:36:54 +0600

    create delete api for single product

--- index.js
+++ index.js
@@ -55,6 +55,13 @@ async function run() {
             res.send(result);
         });
 
+        // delet single products api 
+        app.delete('/partses/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await partsCollection.deleteOne(query);
+            res.send(result);
+        });
 
         // get single parts API
         app.get('/partses/:id', async (req, res) => {
